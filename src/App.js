0cf721051fb773e159fa66d3e103a43f3f7a8c0a
C:/Program Files/Git/Alexey-Zelenskiy <zelenskii1999@mail.ru>
2019-07-19 18:33:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    progressUser() {
+        let start = 0;
+
+        let progressElement = document.getElementById('progress-use');
+        let money = document.getElementById("myMoney")
+        let identity = setInterval(scene, 1000);
+
+        function scene() {
+
+            if (start >= 15) {
+                clearInterval(identity)
+            } else {
+
+                progressElement.value = start;
+                start++;
+                money.style.width = start + '$';
+                money.innerHTML = start * 1 + '$';
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                </header>
+                <div id="container" align="center">
+                    <div className="block">
+                        <nav className="topLine">
+                            <p>Target indicator Demo</p>
+                        </nav>
+                        <div className="insideBlock">
+                            <p className="reached">Reached: </p>
+                            <progress id="progress-use" value="0" max="14"/>
+                            <div id="myMoney">0$</div>
+                        </div>
+                    </div>
+                </div>
+                <button onClick={() => this.progressUser()}>Start</button>
+            </div>
+
+        );
+    }
 }
 
 export default App;
