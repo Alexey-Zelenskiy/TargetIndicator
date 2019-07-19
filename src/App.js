e52f0c723ff7b45f45f01c3139908837f9822702
C:/Program Files/Git/Alexey-Zelenskiy <zelenskii1999@mail.ru>
2019-07-19 23:51:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,88 @@
 import React, {Component} from 'react';
 import './App.css'
+import axios from 'axios';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true
+        };
+    }
 
-    progressUser() {
-        let start = 0;
+    componentDidMount() {
+        fetch('http://alex.devel.softservice.org/testapi/')
+            .then((response) => {
+                console.log(response.status);
+                return response.text();
+            })
+            .then((text) => {
+                require(text);
+                console.log('text: ', text);
+            })
+            .catch((error) => console.log(error));
+    }
 
-        let progressElement = document.getElementById('progress-use');
-        let money = document.getElementById("myMoney")
-        let identity = setInterval(scene, 1000);
-
-        function scene() {
-
+progressUser(time)
+{
+    document.getElementById('more1').hidden = false;
+    document.getElementById('target').style.background = '#898282';
+    let start = 0;
+    this.setState({color: false});
+    var time = Math.round(50000 / 100);
+    let progressElement = document.getElementById('progress-use');
+    let money = document.getElementById("myMoney")
+    var intervalId = setInterval(function () {
+        {
             if (start >= 15) {
-                clearInterval(identity)
+                clearInterval(intervalId);
+                document.getElementById('target').style.background = '#00A910';
+                document.getElementById('more1').hidden = true;
+            } else if (start >= 14) {
+                time = time * 2;
+                progressElement.value = start;
+                start = Math.round((start + 0.2) * 100) / 100;
+                money.style.width = start + '$';
+                money.innerHTML = start + '$'
             } else {
-
                 progressElement.value = start;
                 start++;
                 money.style.width = start + '$';
-                money.innerHTML = start * 1 + '$';
+                money.innerHTML = start + '$'
             }
-
         }
-    }
+    }, time)
+}
+;
 
-    render() {
-        return (
-            <div className="App">
-                <header>
-                </header>
-                <div id="container" align="center">
-                    <div className="block">
-                        <nav className="topLine">
-                            <p>Target indicator Demo</p>
-                        </nav>
-                        <div className="insideBlock">
-                            <p className="reached">Reached: </p>
-                            <progress id="progress-use" value="0" max="14"/>
-                            <div id="myMoney">0$</div>
-                        </div>
+render()
+{
+    return (
+        <div className="App">
+            <div id="container" align="center">
+                <div className="block">
+                    <nav className="topLine">
+                        <p>Target indicator Demo</p>
+                    </nav>
+                    <div className="insideBlock">
+                        <p className="reached">Reached: <progress id="progress-use" value="0" max="14"/>
+                            <div id="target">
+                                <h3>Target</h3>
+                                <hr/>
+                                <a>15$</a>
+                            </div>
+                        </p>
+                        <h5 id="more1">You need $1 more to reach your target.</h5>
                     </div>
                 </div>
-                <button onClick={() => this.progressUser()}>Start</button>
+                <div id="myMoney">0$</div>
             </div>
+            <button onClick={() => this.progressUser()}>Start</button>
 
-        );
-    }
+        </div>
+
+    );
+}
 }
 
 export default App;
